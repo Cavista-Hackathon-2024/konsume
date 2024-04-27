8d986cbbf6e1569625b55109956108c5a1b1d8ab
segunojo1 <olusegunkd2017@gmail.com>
2024-04-27 08:43:14 +0100

    dashbord update

--- src/components/dashboard/DashboardBody.jsx
+++ src/components/dashboard/DashboardBody.jsx
@@ -2,6 +2,8 @@ import React, { useContext, useEffect, useState } from 'react'
 import star from "../../assets/star2.png"
 import star3 from "../../assets/star3.png"
 import star4 from "../../assets/star4.png"
+import scanner from "../../assets/scannerr.png"
+import progress from "../../assets/progresss.png"
 import gemini from '../../https/gemini'
 import { toast } from 'react-toastify'
 import SetupContext from '../../context/SetupContext'
@@ -10,11 +12,11 @@ import VanillaTilt from 'vanilla-tilt';
 const DashboardBody = () => {
     const { name, age, weight, userGoal, userDiseases } = useContext(SetupContext);
     // const stringdisease = userDiseases.join(, )
-    const qq = `My name is ${name} I am ${age} years old and ${weight}kg, I am working on ${userGoal}, my health conditons are ${userDiseases.join(', ')}. Please give me like 3 or more recommended protein sources. Not more than 30 words`;
+    const qq = `My name is ${name} I am ${age} years old and ${weight}kg, I am working on ${userGoal}, my health conditons are ${userDiseases.join(', ')}. Please give me like 3 or more recommended food sources. Not more than 30 words`;
     const qq2 = `My name is ${name} I am ${age} years old and ${weight}kg, I am working on ${userGoal}, my health conditons are ${userDiseases.join(', ')}. Please give me like 3 or more types of food i should avoid. Not more than 30 words`;
-    const proq = `My name is ${name} I am ${age} years old and ${weight}kg, I am working on ${userGoal}, my health conditons are ${userDiseases.join(', ')}. What is the recommended range of percent of protein i should eat, it must be exactly in this format i.e 30-45%`;
-    const carbq = `My name is ${name} I am ${age} years old and ${weight}kg, I am working on ${userGoal}, my health conditons are ${userDiseases.join(', ')}. What is the recommended range of percent of fats i should eat, it must be exactly in this format i.e 30-45%`;
-    const fatq = `My name is ${name} I am ${age} years old and ${weight}kg, I am working on ${userGoal}, my health conditons are ${userDiseases.join(', ')}. What is the recommended range of percent of carbs i should eat, it must be exactly in this format i.e 30-45%`;
+    const proq = `My name is ${name} I am ${age} years old and ${weight}kg, I am working on ${userGoal}, my health conditons are ${userDiseases.join(', ')}. What is the recommended range of percent of protein i should eat, it must be exactly in this format i.e 30-45% do not include anything apart from the percentage`;
+    const carbq = `My name is ${name} I am ${age} years old and ${weight}kg, I am working on ${userGoal}, my health conditons are ${userDiseases.join(', ')}. What is the recommended range of percent of fats i should eat, it must be exactly in this format i.e 30-45% do not include anything apart from the percentage`;
+    const fatq = `My name is ${name} I am ${age} years old and ${weight}kg, I am working on ${userGoal}, my health conditons are ${userDiseases.join(', ')}. What is the recommended range of percent of carbs i should eat, it must be exactly in this format i.e 30-45% do not include anything apart from the percentage`;
     const [query, setQuery] = useState(qq);
     const [query2, setQuery2] = useState(qq2);
     const [query3, setQuery3] = useState(proq);
@@ -23,7 +25,7 @@ const DashboardBody = () => {
 
     const [answer, setAnswer] = useState();
     const [answer2, setAnswer2] = useState();
-    
+
     const [proteinPercent, setProteinPercent] = useState();
     const [carbPercent, setCarbPercent] = useState();
     const [fats, setFats] = useState();
@@ -63,12 +65,12 @@ const DashboardBody = () => {
         }
     }
 
-    
+
 
     const element = document.querySelectorAll(".js-tilt");
     VanillaTilt.init(element);
     return (
-        <div className='grid grid-cols-3'>
+        <div className='grid grid-cols-3 gap-3'>
             <div className='grid grid-rows-2 gap-2'>
                 <div className='bg-[#8C77EC] p-5 gap-2 flex flex-col rounded-2xl js-tilt'>
                     <div className='flex justify-between items-center'>
@@ -108,23 +110,39 @@ const DashboardBody = () => {
                     </div>
                 </div>
             </div>
-            <div>
-                <button className='p-7'>Scan Image</button>
+            <div className='grid grid-rows-2 gap-2'>
+                <div className='bg-[#B0D2C1] p-4 rounded-2xl js-tilt'>
+                    <div className='flex justify-between items-center mb-5'>
+                        <p className=' font-bold text-xs text-[#ED5E3B]'>Try Out Scanner</p>
+                        <img src={scanner} alt="scanner" />
+                    </div>
+                    <p>Upload a food picture, and our AI analyzes it for nutritional info, allergens, and health goal alignment.</p>
+                </div>
+
+                <div className='border-[2px] border-[#FFC400] p-4 rounded-2xl js-tilt'>
+                    <div className='flex justify-between items-center mb-5'>
+                        <p className=' font-bold text-base'>Progress Tracker</p>
+                        <img src={progress} alt="progress" />
+                    </div>
+                    <p>Track your meals, goals and workout progress? <br/>
+Try out our Beta Project tracker coming Soon!</p>
+                </div>
             </div>
+
             <div className='grid grid-rows-2 gap-2'>
                 <div className='bg-[#D6FBC4] p-4 rounded-2xl js-tilt'>
                     <div className='flex justify-between items-center mb-5'>
-                        <p className=' font-bold text-xs text-[#8C77EC]'>Recommended Protein Sources</p>
+                        <p className=' font-bold text-xs text-[#8C77EC]'>Recommended Food Sources</p>
                         <img src={star3} alt="star" />
                     </div>
-                    <ul className=' text-xs font-medium'>
+                    <ul className=' text-xs font-medium text-[#286711]'>
                         {answer}
                     </ul>
                 </div>
 
                 <div className='bg-[#FF004D] p-4 rounded-2xl js-tilt'>
                     <div className='flex justify-between items-center mb-5'>
-                        <p className=' font-bold text-xs'>Foods to avoid</p>
+                        <p className=' font-bold text-xs'>Food to avoid</p>
                         <img src={star4} alt="star" />
                     </div>
                     <ul className=' text-xs font-medium'>
