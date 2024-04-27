bfa53ec3e2299d549394ef516de44d4419d51e71
segunojo1 <olusegunkd2017@gmail.com>
2024-04-27 04:59:29 +0100

    feat: Gemini ai api integration

--- src/components/setup/GoalCheckbox.jsx
+++ src/components/setup/GoalCheckbox.jsx
@@ -22,7 +22,7 @@ const GoalCheckbox = ({ label,data }) => {
         value={label} 
         name="inline-radio-group" 
         checked={userGoal === label}
-        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600" />
+        class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 dark:ring-offset-gray-800 dark:bg-gray-700 dark:border-gray-600" />
         <label for="inline-radio" class="text-lg text-gray-600 ml-2 font-bold font-jakarta">{label}</label>
       </div>
     </div>
