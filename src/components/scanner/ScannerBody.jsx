97ec2bf5b84443a8be327a64984ea9ebca643432
segunojo1 <olusegunkd2017@gmail.com>
2024-05-02 18:22:07 +0100

    scanner

--- src/components/scanner/ScannerBody.jsx
+++ src/components/scanner/ScannerBody.jsx
@@ -1,15 +1,16 @@
 import { GoogleGenerativeAI, HarmBlockThreshold, HarmCategory } from '@google/generative-ai';
 import React, { useContext, useState } from 'react'
 import SetupContext from '../../context/SetupContext';
+import upload from "../../assets/upload.png"
 
 const ScannerBody = () => {
-    const API_KEY = import.meta.env.VITE_GEMINI_KEY;
-    const genAI = new GoogleGenerativeAI(API_KEY);
+  const API_KEY = import.meta.env.VITE_GEMINI_KEY;
+  const genAI = new GoogleGenerativeAI(API_KEY);
 
-    const [image, setImage] = useState(null);
+  const [image, setImage] = useState(null);
   const [bitImage, setBitImage] = useState('');
   const [result, setResult] = useState('');
-  const {userGoal, setUserGoal, userDiseases, setUserDiseases,name, age, gender, weight, setAge, setGender, setWeight, setName} = useContext(SetupContext);
+  const { userGoal, setUserGoal, userDiseases, setUserDiseases, name, age, gender, weight, setAge, setGender, setWeight, setName } = useContext(SetupContext);
   const [queryText, setQueryText] = useState(`Whats in this image? Is it a food? What is in this food? with my information like my goal of ${userGoal}, health conditions i have like ${userDiseases.join(', ')}`);
 
   const safetySettings = [
@@ -39,34 +40,34 @@ const ScannerBody = () => {
     }).catch(e => console.log(e))
 
     generativeFile(file).then(
-        (img) => {
-            setBitImage(img);
-        }
+      (img) => {
+        setBitImage(img);
+      }
     )
   };
 
-    const generativeFile = async (file) => {
-        const base64EncodedDataPromise = new Promise((resolve) => {
-            const reader = new FileReader();
-            reader.onloadend = () => resolve(reader.result.split(',')[1]);
-            reader.readAsDataURL(file);
-        });
+  const generativeFile = async (file) => {
+    const base64EncodedDataPromise = new Promise((resolve) => {
+      const reader = new FileReader();
+      reader.onloadend = () => resolve(reader.result.split(',')[1]);
+      reader.readAsDataURL(file);
+    });
 
-        return {
-            inlineData: { data: await base64EncodedDataPromise, mimeType: file.type },
-        };
-    }
-    const getBase64 = (file) => new Promise(function (resolve, reject) {
+    return {
+      inlineData: { data: await base64EncodedDataPromise, mimeType: file.type },
+    };
+  }
+  const getBase64 = (file) => new Promise(function (resolve, reject) {
     let reader = new FileReader();
     reader.readAsDataURL(file);
     reader.onload = () => resolve(reader.result)
     reader.onerror = (error) => reject('Error: ', error);
-})
+  })
 
   const handleRecognize = async () => {
     const model = genAI.getGenerativeModel({ model: "gemini-pro-vision" });
     const result = await model.generateContent([
-        queryText, bitImage
+      queryText, bitImage
     ], safetySettings);
     const response = await result.response;
     const text = response.text();
@@ -74,13 +75,23 @@ const ScannerBody = () => {
   };
   return (
     <div>
+      <div className='border-[2.6px] border-[#097F5B] flex flex-col md:flex-row gap-3 items-center justify-between bg-[#E0F6EF] md:w-[450px] py-6 rounded-[19.7px] mx-auto'>
+        <img src={image} alt="img" className='w-[150px] rounded-md flex-[.5]' />
 
-    <img src={image} alt="img" className='w-[150px] rounded-md'/>
-    <div className='my-10 w-fit flex mx-auto '>
-        <input type="file" onChange={handleImageChange} />
-        <button className=' p-4 w-fit bg-[#B0D2C1] mx-auto' onClick={handleRecognize} >Recognize Image</button>
-    </div>
-    <p>Result: {result}</p>
+        <div className='flex flex-col gap-4 items-center flex-[.5] border border-[grey]'>
+          <img src={upload} alt="upload" />
+          <p>Upload Image.</p>
+          <p>Drag and Drop files or...</p>
+          <div class="file-input-wrapper">
+            <input type="file" id="fileInput" onChange={handleImageChange} />
+            <label class="file-input-label" for="fileInput">Choose a file</label>
+          </div>
+          <div id="fileName"></div>
+        </div>
+
+      </div>
+      <button className=' p-4 w-fit bg-[#B0D2C1] mx-auto flex mt-3' onClick={handleRecognize} >Recognize Image</button>
+      <p>Result: {result}</p>
     </div>
   )
 }
--- src/pages/Scanner.jsx
+++ src/pages/Scanner.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Sidebar from '../components/dashboard/Sidebar'
 import ScannerHead from '../components/scanner/ScannerHead'
 import ScannerBody from '../components/scanner/ScannerBody'
+import DashboardNav from '../components/dashboard/DashboardNav'
 
 const Scanner = () => {
+  const [toggled, setToggled] = useState(false);
   return (
     <div>
-        <Sidebar />
-        <div className='ml-[280px] mr-[20px] gap-5 flex flex-col'>
-            <ScannerHead />
-            {/* <DashboardBody /> */}
-            <ScannerBody />
-        </div>
+      <Sidebar toggled={toggled} setToggled={setToggled} />
+      <div className={`${toggled ? '' : "md:ml-[280px]"} gap-5 flex flex-col px-5`}>
+        <DashboardNav toggled={toggled} setToggled={setToggled} />
+        <ScannerHead />
+        {/* <DashboardBody /> */}
+        <ScannerBody />
+      </div>
     </div>
   )
 }
